6a3a3fa967fd604968197dc750b7dcb8c3facb77
hitana.cpp <hitana.cpp@gmail.com>
2020-01-12 12:45:47 -0500

    [+] Add D files

---
+++ src/qdldl_types.d
@@ -0,0 +1,14 @@
+
+module qdldl_types;
+
+import core.stdc.limits;
+
+// QDLDL types
+
+alias QDLDL_BOOL_TYPE = ubyte;
+
+alias QDLDL_int = int;
+alias QDLDL_float = double;
+alias QDLDL_bool = ubyte;
+
+enum long QDLDL_INT_MAX = INT_MAX;
