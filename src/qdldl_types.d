200ac8ae77920dbc94b0b4ac4155c0b8e9b57dc6
hitana.cpp <hitana.cpp@gmail.com>
2020-02-06 01:00:14 -0500

    [*] Replace configuring QDLDL types with D versions

--- src/qdldl_types.d
+++ src/qdldl_types.d
@@ -5,10 +5,26 @@ import core.stdc.limits;
 
 // QDLDL types
 
+/* Use customized number representation -----------------------------------   */
+version (DLONG)
+{
+  alias QDLDL_int = long;
+  enum long QDLDL_INT_MAX = LLONG_MAX;
+
+}
+else { // standard integers
+  alias QDLDL_int = int;       /* for indices */
+  enum long QDLDL_INT_MAX = INT_MAX;
+}
+
+version (DFLOAT)
+{
+  alias QDLDL_float = float;  /* for numerical values  */
+}
+else {
+  alias QDLDL_float = double;
+}
+
+
 alias QDLDL_BOOL_TYPE = ubyte;
-
-alias QDLDL_int = int;
-alias QDLDL_float = double;
 alias QDLDL_bool = ubyte;
-
-enum long QDLDL_INT_MAX = INT_MAX;
